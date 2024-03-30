d69361f8185dc7c4aad47f39bf76c868f31d8578
peter <peter@paperdog.com>
2024-03-30 20:11:09 +0100

    Refactor navigation component to use MenuItem model
    The navigation component has been refactored to use a new 'MenuItem' model class. This new class, which includes property fields for name, url, and potential child menu items, replaces the previous 'FoodNode' interface. The 'getMenuItems' method from AuthorizationDataService is now used to provide data for the dataSource.

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,38 +1,15 @@
 import {Component} from '@angular/core';
 import {MatTreeFlatDataSource, MatTreeFlattener} from "@angular/material/tree";
 import {FlatTreeControl} from "@angular/cdk/tree";
-
-
-interface FoodNode {
-  name: string;
-  children?: FoodNode[];
-}
-
-const TREE_DATA: FoodNode[] = [
-  {
-    name: 'Fruit',
-    children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
-  },
-  {
-    name: 'Vegetables',
-    children: [
-      {
-        name: 'Green',
-        children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
-      },
-      {
-        name: 'Orange',
-        children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
-      },
-    ],
-  },
-];
+import {MenuItem} from "../models/MenuItem";
+import {AuthorizationDataService} from "../services/authorization-data.service";
 
 /** Flat node with expandable and level information */
-interface ExampleFlatNode {
+interface FlatNode {
   expandable: boolean;
   name: string;
   level: number;
+  url: string | undefined;
 }
 
 @Component({
@@ -41,15 +18,16 @@ interface ExampleFlatNode {
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
-  private _transformer = (node: FoodNode, level: number) => {
+  private _transformer = (node: MenuItem, level: number) => {
     return {
       expandable: !!node.children && node.children.length > 0,
       name: node.name,
       level: level,
+      url: node.url
     };
   };
 
-  treeControl = new FlatTreeControl<ExampleFlatNode>(
+  treeControl = new FlatTreeControl<FlatNode>(
     node => node.level,
     node => node.expandable,
   );
@@ -63,9 +41,10 @@ export class NavigationComponent {
 
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
-  constructor() {
-    this.dataSource.data = TREE_DATA;
+  constructor(private authSrv: AuthorizationDataService) {
+    this.dataSource.data = authSrv.getMenuItems();
+    console.log(this.dataSource.data)
   }
 
-  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+  hasChild = (_: number, node: FlatNode) => node.expandable;
 }

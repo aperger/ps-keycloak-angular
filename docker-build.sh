3b4e548144245031b24b81b547f0f24d88629288
Perger Attila <perger.attila@pergersoft.hu>
2024-12-18 16:06:11 +0100

    Fixes build

--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,14 @@
 #!/bin/ksh
-docker build --tag ps-keycloak-angular:latest .
+. src/environments/.env
+
+echo KEYCLOAK_URL=$KEYCLOAK_URL
+echo KEYCLOAK_REALM=$KEYCLOAK_REALM
+echo KEYCLOAK_CLIENT_ID=$KEYCLOAK_CLIENT_ID
+echo AXING_API_URL=$AXING_API_URL
+
+docker build \
+    --build-arg KEYCLOAK_URL=$KEYCLOAK_URL \
+    --build-arg KEYCLOAK_REALM=$KEYCLOAK_REALM \
+    --build-arg KEYCLOAK_CLIENT_ID=$KEYCLOAK_CLIENT_ID \
+    --build-arg AXING_API_URL=$AXING_API_URL \
+    --tag ps-keycloak-angular:latest .
